9f274cc66497d080aa4f15d3024514a707aeb22e
GyeongHwa <gyeonghwaa_@naver.com>
2024-08-28 16:01:26 +0900

    5. BCrypt 암호화 메소드

--- src/main/java/com/example/TestSecurity/config/SecurityConfig.java
+++ src/main/java/com/example/TestSecurity/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -31,5 +32,12 @@ public class SecurityConfig {
                         .disable());
 
         return http.build();
+    }
+
+
+    //bcrypt
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 }
