d4e009babc37d3e9a36eed4ef47d89d7635a5366
GyeongHwa <gyeonghwaa_@naver.com>
2024-08-29 09:59:35 +0900

    5. SecurityConfig 등록

--- src/main/java/com/example/TestSecurity/controller/MainController.java
+++ src/main/java/com/example/TestSecurity/controller/MainController.java
@@ -1,4 +1,4 @@
-package com.example.TestSecurity;
+package com.example.TestSecurity.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
